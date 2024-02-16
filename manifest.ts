ae274f85a9fcd3f210db07a7cdb61950b5585d17
capi1O <18724661+capi1O@users.noreply.github.com>
2024-02-16 17:55:53 +0100

    use sized images for popup icon

--- manifest.ts
+++ manifest.ts
@@ -38,7 +38,12 @@ export default defineManifest(async () => ({
 		128: 'images/icon-128.png',
 	},
 	action: {
-		default_icon: 'logo.png',
+		default_icon: {
+			16: 'images/icon-16.png',
+			32: 'images/icon-32.png',
+			48: 'images/icon-48.png',
+			128: 'images/icon-128.png',
+		},
 		default_popup: 'src/popup.html',
 		default_title: '__MSG_extensionName__ settings',
 	},
